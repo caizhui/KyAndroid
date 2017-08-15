57470dcbba1a4b7d008ca77d3e32a37661d1b85c
kid236901 <435068610@qq.com>
2017-08-16 00:07:40 +0800

    1.4.6.10

--- app/src/main/java/com/ky/kyandroid/util/DateTimePickDialogUtil.java
+++ app/src/main/java/com/ky/kyandroid/util/DateTimePickDialogUtil.java
@@ -9,182 +9,230 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TimePicker;
 import android.widget.TimePicker.OnTimeChangedListener;
+import android.widget.Toast;
 
 import com.ky.kyandroid.R;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 /**
  * 日期时间选择控件 使用方法： private EditText inputDate;//需要设置的日期时间文本编辑框 private String
  * initDateTime="2012年9月3日 14:44",//初始日期时间值 在点击事件中使用：
  * inputDate.setOnClickListener(new OnClickListener() {
  *
- * @Override public void onClick(View v) { DateTimePickDialogUtil
- *           dateTimePicKDialog=new
- *           DateTimePickDialogUtil(SinvestigateActivity.this,initDateTime);
- *           dateTimePicKDialog.dateTimePicKDialog(inputDate);
- *
- *           } });
- *
  * @author
+ * @Override public void onClick(View v) { DateTimePickDialogUtil
+ * dateTimePicKDialog=new
+ * DateTimePickDialogUtil(SinvestigateActivity.this,initDateTime);
+ * dateTimePicKDialog.dateTimePicKDialog(inputDate);
+ * <p>
+ * } });
  */
 public class DateTimePickDialogUtil implements OnDateChangedListener,
-		OnTimeChangedListener {
-	private DatePicker datePicker;
-	private TimePicker timePicker;
-	private AlertDialog ad;
-	private String dateTime;
-	private String initDateTime;
-	private Activity activity;
+        OnTimeChangedListener {
+    private DatePicker datePicker;
+    private TimePicker timePicker;
+    private AlertDialog ad;
+    private String dateTime;
+    private String initDateTime;
+    private Activity activity;
 
-	/**
-	 * 日期时间弹出选择框构造函数
-	 *
-	 * @param activity
-	 *            ：调用的父activity
-	 * @param initDateTime
-	 *            初始日期时间值，作为弹出窗口的标题和日期时间初始值
-	 */
-	public DateTimePickDialogUtil(Activity activity, String initDateTime) {
-		this.activity = activity;
-		this.initDateTime = initDateTime;
+    /**
+     * 日期时间弹出选择框构造函数
+     *
+     * @param activity     ：调用的父activity
+     * @param initDateTime 初始日期时间值，作为弹出窗口的标题和日期时间初始值
+     */
+    public DateTimePickDialogUtil(Activity activity, String initDateTime) {
+        this.activity = activity;
+        this.initDateTime = initDateTime;
 
-	}
+    }
 
-	public void init(DatePicker datePicker, TimePicker timePicker) {
-		Calendar calendar = Calendar.getInstance();
-		if (!(null == initDateTime || "".equals(initDateTime))) {
-			calendar = this.getCalendarByInintData(initDateTime);
-		} else {
-			initDateTime = calendar.get(Calendar.YEAR) + "-"
-					+ calendar.get(Calendar.MONTH) + "-"
-					+ calendar.get(Calendar.DAY_OF_MONTH)
-					+ calendar.get(Calendar.HOUR_OF_DAY) + ":"
-					+ calendar.get(Calendar.MINUTE);
-		}
+    public void init(DatePicker datePicker, TimePicker timePicker) {
+        Calendar calendar = Calendar.getInstance();
+        if (!(null == initDateTime || "".equals(initDateTime))) {
+            calendar = this.getCalendarByInintData(initDateTime);
+        } else {
+            initDateTime = calendar.get(Calendar.YEAR) + "-"
+                    + calendar.get(Calendar.MONTH) + "-"
+                    + calendar.get(Calendar.DAY_OF_MONTH)
+                    + calendar.get(Calendar.HOUR_OF_DAY) + ":"
+                    + calendar.get(Calendar.MINUTE);
+        }
 
-		datePicker.init(calendar.get(Calendar.YEAR),
-				calendar.get(Calendar.MONTH),
-				calendar.get(Calendar.DAY_OF_MONTH), this);
-		timePicker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
-		timePicker.setCurrentMinute(calendar.get(Calendar.MINUTE));
-	}
+        datePicker.init(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH), this);
+        timePicker.setCurrentHour(calendar.get(Calendar.HOUR_OF_DAY));
+        timePicker.setCurrentMinute(calendar.get(Calendar.MINUTE));
+    }
 
-	/**
-	 * 弹出日期时间选择框方法
-	 *
-	 * @param inputDate
-	 *            :为需要设置的日期时间文本编辑框
-	 * @return
-	 */
-	public AlertDialog dateTimePicKDialog(final EditText inputDate) {
-		LinearLayout dateTimeLayout = (LinearLayout) activity
-				.getLayoutInflater().inflate(R.layout.common_datetime, null);
-		datePicker = (DatePicker) dateTimeLayout.findViewById(R.id.datepicker);
-		timePicker = (TimePicker) dateTimeLayout.findViewById(R.id.timepicker);
-		init(datePicker, timePicker);
-		timePicker.setIs24HourView(true);
-		timePicker.setOnTimeChangedListener(this);
+    /**
+     * 弹出日期时间选择框方法
+     *
+     * @param inputDate :为需要设置的日期时间文本编辑框
+     * @return
+     */
+    public AlertDialog dateTimePicKDialog(final EditText inputDate) {
+        LinearLayout dateTimeLayout = (LinearLayout) activity
+                .getLayoutInflater().inflate(R.layout.common_datetime, null);
+        datePicker = (DatePicker) dateTimeLayout.findViewById(R.id.datepicker);
+        timePicker = (TimePicker) dateTimeLayout.findViewById(R.id.timepicker);
+        init(datePicker, timePicker);
+        timePicker.setIs24HourView(true);
+        timePicker.setOnTimeChangedListener(this);
 
-		ad = new AlertDialog.Builder(activity)
-				.setTitle(initDateTime)
-				.setView(dateTimeLayout)
-				.setPositiveButton("确定", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int whichButton) {
-						inputDate.setText(dateTime);
-					}
-				})
-				.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int whichButton) {
-						inputDate.setText("");
-					}
-				}).show();
+        ad = new AlertDialog.Builder(activity)
+                .setTitle(initDateTime)
+                .setView(dateTimeLayout)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        inputDate.setText(dateTime);
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        inputDate.setText("");
+                    }
+                }).show();
 
-		onDateChanged(null, 0, 0, 0);
-		return ad;
-	}
+        onDateChanged(null, 0, 0, 0);
+        return ad;
+    }
 
-	public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
-		onDateChanged(null, 0, 0, 0);
-	}
+    /**
+     * 弹出日期时间选择框方法
+     *
+     * @param inputDate :为需要设置的日期时间文本编辑框
+     * @return
+     */
+    public AlertDialog dateTimePicKDialog(final EditText inputDate, final boolean istoday) {
+        LinearLayout dateTimeLayout = (LinearLayout) activity
+                .getLayoutInflater().inflate(R.layout.common_datetime, null);
+        datePicker = (DatePicker) dateTimeLayout.findViewById(R.id.datepicker);
+        timePicker = (TimePicker) dateTimeLayout.findViewById(R.id.timepicker);
+        init(datePicker, timePicker);
+        timePicker.setIs24HourView(true);
+        timePicker.setOnTimeChangedListener(this);
 
-	public void onDateChanged(DatePicker view, int year, int monthOfYear,
-							  int dayOfMonth) {
-		// 获得日历实例
-		Calendar calendar = Calendar.getInstance();
+        ad = new AlertDialog.Builder(activity)
+                .setTitle(initDateTime)
+                .setView(dateTimeLayout)
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                    if(istoday){
+                        Calendar curn = Calendar.getInstance();
+                        curn.setTime(new Date());
+                        curn.add(Calendar.DATE, 1);//增加一天
+                        try {
+                            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+                            Date selectTime = sdf.parse(dateTime);
+                            Date currentTime = sdf.parse(sdf.format(curn.getTime()));
+                            if(currentTime.getTime() > selectTime.getTime()){
+                                inputDate.setText(dateTime);
+                            }else{
+                                Toast.makeText(activity,"发生时间不能大于当前时间",Toast.LENGTH_SHORT).show();
+                                inputDate.setText("");
+                            }
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                            inputDate.setText("");
+                        }
+                    }
 
-		calendar.set(datePicker.getYear(), datePicker.getMonth(),
-				datePicker.getDayOfMonth(), timePicker.getCurrentHour(),
-				timePicker.getCurrentMinute());
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                    }
+                })
+                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        inputDate.setText("");
+                    }
+                }).show();
 
-		dateTime = sdf.format(calendar.getTime());
-		ad.setTitle(dateTime);
-	}
+        onDateChanged(null, 0, 0, 0);
+        return ad;
+    }
 
-	/**
-	 * 实现将初始日期时间2012年07月02日 16:45 拆分成年 月 日 时 分 秒,并赋值给calendar
-	 *
-	 * @param initDateTime
-	 *            初始日期时间值 字符串型
-	 * @return Calendar
-	 */
-	private Calendar getCalendarByInintData(String initDateTime) {
-		Calendar calendar = Calendar.getInstance();
+    public void onTimeChanged(TimePicker view, int hourOfDay, int minute) {
+        onDateChanged(null, 0, 0, 0);
+    }
 
-		// 将初始日期时间2012年07月02日 16:45 拆分成年 月 日 时 分 秒
-		String date = spliteString(initDateTime, "日", "index", "front"); // 日期
-		String time = spliteString(initDateTime, "日", "index", "back"); // 时间
+    public void onDateChanged(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+        // 获得日历实例
+        Calendar calendar = Calendar.getInstance();
 
-		String yearStr = spliteString(date, "年", "index", "front"); // 年份
-		String monthAndDay = spliteString(date, "年", "index", "back"); // 月日
+        calendar.set(datePicker.getYear(), datePicker.getMonth(),
+                datePicker.getDayOfMonth(), timePicker.getCurrentHour(),
+                timePicker.getCurrentMinute());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-		String monthStr = spliteString(monthAndDay, "月", "index", "front"); // 月
-		String dayStr = spliteString(monthAndDay, "月", "index", "back"); // 日
+        dateTime = sdf.format(calendar.getTime());
+        ad.setTitle(dateTime);
+    }
 
-		String hourStr = spliteString(time, ":", "index", "front"); // 时
-		String minuteStr = spliteString(time, ":", "index", "back"); // 分
+    /**
+     * 实现将初始日期时间2012年07月02日 16:45 拆分成年 月 日 时 分 秒,并赋值给calendar
+     *
+     * @param initDateTime 初始日期时间值 字符串型
+     * @return Calendar
+     */
+    private Calendar getCalendarByInintData(String initDateTime) {
+        Calendar calendar = Calendar.getInstance();
 
-		int currentYear = Integer.valueOf(yearStr.trim()).intValue();
-		int currentMonth = Integer.valueOf(monthStr.trim()).intValue() - 1;
-		int currentDay = Integer.valueOf(dayStr.trim()).intValue();
-		int currentHour = Integer.valueOf(hourStr.trim()).intValue();
-		int currentMinute = Integer.valueOf(minuteStr.trim()).intValue();
+        // 将初始日期时间2012年07月02日 16:45 拆分成年 月 日 时 分 秒
+        String date = spliteString(initDateTime, "日", "index", "front"); // 日期
+        String time = spliteString(initDateTime, "日", "index", "back"); // 时间
 
-		calendar.set(currentYear, currentMonth, currentDay, currentHour,
-				currentMinute);
-		return calendar;
-	}
+        String yearStr = spliteString(date, "年", "index", "front"); // 年份
+        String monthAndDay = spliteString(date, "年", "index", "back"); // 月日
 
-	/**
-	 * 截取子串
-	 *
-	 * @param srcStr
-	 *            源串
-	 * @param pattern
-	 *            匹配模式
-	 * @param indexOrLast
-	 * @param frontOrBack
-	 * @return
-	 */
-	public static String spliteString(String srcStr, String pattern,
-									  String indexOrLast, String frontOrBack) {
-		String result = "";
-		int loc = -1;
-		if (indexOrLast.equalsIgnoreCase("index")) {
-			loc = srcStr.indexOf(pattern); // 取得字符串第一次出现的位置
-		} else {
-			loc = srcStr.lastIndexOf(pattern); // 最后一个匹配串的位置
-		}
-		if (frontOrBack.equalsIgnoreCase("front")) {
-			if (loc != -1)
-				result = srcStr.substring(0, loc); // 截取子串
-		} else {
-			if (loc != -1)
-				result = srcStr.substring(loc + 1, srcStr.length()); // 截取子串
-		}
-		return result;
-	}
+        String monthStr = spliteString(monthAndDay, "月", "index", "front"); // 月
+        String dayStr = spliteString(monthAndDay, "月", "index", "back"); // 日
+
+        String hourStr = spliteString(time, ":", "index", "front"); // 时
+        String minuteStr = spliteString(time, ":", "index", "back"); // 分
+
+        int currentYear = Integer.valueOf(yearStr.trim()).intValue();
+        int currentMonth = Integer.valueOf(monthStr.trim()).intValue() - 1;
+        int currentDay = Integer.valueOf(dayStr.trim()).intValue();
+        int currentHour = Integer.valueOf(hourStr.trim()).intValue();
+        int currentMinute = Integer.valueOf(minuteStr.trim()).intValue();
+
+        calendar.set(currentYear, currentMonth, currentDay, currentHour,
+                currentMinute);
+        return calendar;
+    }
+
+    /**
+     * 截取子串
+     *
+     * @param srcStr      源串
+     * @param pattern     匹配模式
+     * @param indexOrLast
+     * @param frontOrBack
+     * @return
+     */
+    public static String spliteString(String srcStr, String pattern,
+                                      String indexOrLast, String frontOrBack) {
+        String result = "";
+        int loc = -1;
+        if (indexOrLast.equalsIgnoreCase("index")) {
+            loc = srcStr.indexOf(pattern); // 取得字符串第一次出现的位置
+        } else {
+            loc = srcStr.lastIndexOf(pattern); // 最后一个匹配串的位置
+        }
+        if (frontOrBack.equalsIgnoreCase("front")) {
+            if (loc != -1)
+                result = srcStr.substring(0, loc); // 截取子串
+        } else {
+            if (loc != -1)
+                result = srcStr.substring(loc + 1, srcStr.length()); // 截取子串
+        }
+        return result;
+    }
 
 }
